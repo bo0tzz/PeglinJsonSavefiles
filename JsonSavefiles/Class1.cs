a2d00ce33e1afffee73d853afb6d8cb6b47c8b51
bo0tzz <btdewilligen@gmail.com>
2021-09-26 12:52:22 +0200

    Add project files.

---
+++ JsonSavefiles/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BepInEx;
+using HarmonyLib;
+
+namespace JsonSavefiles
+{
+    [BepInPlugin(modGuid, modName, modVersion)]
+    [BepInProcess("Peglin.exe")]
+    public class JsonSavefiles : BaseUnityPlugin
+    {
+        private const string modGuid = "me.bo0tzz.peglinmods.jsonsavefiles";
+        private const string modName = "JSON Savefiles for Peglin";
+        private const string modVersion = "1.0.0.0";
+        private readonly Harmony harmony = new Harmony(modGuid);
+
+        void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo("JsonSavefiles mod loaded");
+        }
+    }
+}
